850cf9f3e0d5d1c1c764816d3cb3e9781f46d529
Katie Moosman <moosman.katie@gmail.com>
2019-04-05 22:41:13 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
-  resources :authors
+  # resources :posts
+  # resources :authors
 
   resources :authors, only: %i[show new edit create update]
   resources :posts, only: %i[show new edit create update]
